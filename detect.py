42e2f82fc1aec78a2d8125d1e6f617446678f136
Xinlly <Xinlly@outlook.com>
2024-04-10 21:58:52 +0800

    新增关键区域平均温度计算

--- detect.py
+++ detect.py
@@ -2,5 +2,5 @@ import os
 
 os.chdir("yolov5_mod")
 # os.system(r'python ..\.temp\test.py')
-os.system(r'python detect.py --weights ..\models\facialRegion_yolov5s.pt --source ..\datasets\facialRegion\val\images\218.jpg --img 384 --line-thickness 1 --name exp --project ..\results\.temp\detect\facialRegion')
+os.system(r'python detect.py --weights ..\models\facialRegion_yolov5s.pt --source ..\datasets\temper\test\images --img 384 --line-thickness 1 --name exp --project ..\results\detect\.temp\facialRegion --save-txt --hide-conf --show-temper')
 # os.system(r'python detect.py --weights ..\models\facialRegion_yolov5s.pt --source data\images\zidane.jpg --img 1280 --line-thickness 1 --name exp --project ..\results\detect\facialRegion\.temp')
